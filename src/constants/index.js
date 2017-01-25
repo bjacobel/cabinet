8bf9135e7298461e8f11e970af56331cb0871bac
Brian Jacobel <bjacobel@gmail.com>
2017-01-25 12:52:10 -0500

    mhm yes yes totally meant to do that

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const SHOW_DEV_TOOLS = process.env.NODE_ENV !== 'production';
-export const COLLECT_ANALYTICS = process.env.NODE_ENV !== 'production';
+export const COLLECT_ANALYTICS = process.env.NODE_ENV === 'production';
 export const GA_ID = 'UA-34138258-7';
