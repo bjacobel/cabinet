6fa636256402a5cbaea2cefacd7be84173b76e0e
Brian Jacobel <bjacobel@gmail.com>
2019-01-03 13:01:27 -0500

    Bring latest version of cfn templates in

--- aws/utils/launch.js
+++ aws/utils/launch.js
@@ -1,6 +1,6 @@
 const Stack = require('@bjacobel/cloudformer-node');
 
-const template = require('../template');
+const { template, parameters } = require('../template');
 const config = require('../../config');
 
 module.exports = () => {
@@ -8,16 +8,17 @@ module.exports = () => {
   stack.apply(
     template,
     {
-      Parameters: config,
+      Parameters: Object.keys(config)
+        .filter((x) => parameters.includes(x))
+        .reduce((prev, curr) => ({ ...prev, [curr]: config[curr] }), {}),
       Tags: {
         Name: config.ProjectName,
       },
+      Capabilities: ['CAPABILITY_IAM'],
     },
-    console.log
+    (result) => console.log(result)
   );
-  console.log(
-    `Check on the progress of the stack in the AWS console:
+  console.log(`Check on the progress of the stack in the AWS console:
     \rhttps://console.aws.amazon.com/cloudformation/home#/stacks?filter=active\n
-    \rLeaving this process running will tail CloudFormation stack events to the console as well.\n`
-  );
+    \rLeaving this process running will tail CloudFormation stack events to the console as well.\n`);
 };
